9f9b5f49499d674b3300bdb73da17c928e10d666
waryst <w4ryst@gmail.com>
2025-10-03 07:34:48 +0700

    asn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./keycloak/**/*.{html,js,ftl}'],
+  content: ['./keycloak/**/*.{html,js}'],
   theme: {
     extend: {},
   },
